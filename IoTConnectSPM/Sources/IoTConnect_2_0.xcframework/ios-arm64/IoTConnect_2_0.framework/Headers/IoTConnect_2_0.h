7d9d221bd706b06476836e5b53a1246a8eb5bc85
sw-tt-kirtanvaghela <135005218+sw-tt-kirtanvaghela@users.noreply.github.com>
2023-10-18 18:41:28 +0530

    add package

---
+++ IoTConnectSPM/Sources/IoTConnect_2_0.xcframework/ios-arm64/IoTConnect_2_0.framework/Headers/IoTConnect_2_0.h
@@ -0,0 +1,21 @@
+//
+//  IoTConnect_2_0.h
+//  IoTConnect_2.0
+//
+//  Created by kirtan.vaghela on 01/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//#import "GCDAsyncSocket.h"
+//#import "GCDAsyncUdpSocket.h"
+
+//! Project version number for IoTConnect_2_0.
+FOUNDATION_EXPORT double IoTConnect_2_0VersionNumber;
+
+//! Project version string for IoTConnect_2_0.
+FOUNDATION_EXPORT const unsigned char IoTConnect_2_0VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IoTConnect_2_0/PublicHeader.h>
+
+
