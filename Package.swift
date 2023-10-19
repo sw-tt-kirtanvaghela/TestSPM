d1bc638bca23665756a7babf13e537643c122a19
sw-tt-kirtanvaghela <135005218+sw-tt-kirtanvaghela@users.noreply.github.com>
2023-10-19 11:00:39 +0530

    update package file

--- Package.swift
+++ Package.swift
@@ -18,9 +18,18 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "IoTConnectSPM"),
+            name: "IoTConnectSPM",
+            dependencies: [
+                .target(name: "IoTConnect_2_0"),
+                .product(name: "CocoaMQTT", package: "CocoaMQTT"),
+            ]
+        ),
         .testTarget(
             name: "IoTConnectSPMTests",
             dependencies: ["IoTConnectSPM"]),
+        .binaryTarget(
+            name: "IoTConnect_2_0",
+            path: "./Sources/IoTConnect_2_0.xcframework")
     ]
 )
+
